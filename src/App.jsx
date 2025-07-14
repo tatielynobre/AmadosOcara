33057f8f1f191761eea77a6099659a70536bda6b
tatiely <tatiely.lima.nobre@gmail.com>
2025-07-14 19:21:00 -0300

    primeiro commit - Estruturando

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   
+      <div className="card">
+        <h1>Welcome to Amados</h1>
+        <p>Quem somos?</p>
+      </div>
+     
+    
+  )
+}
+
+export default App
