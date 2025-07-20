ae6ed942a2ecc475ef67764cc75cba3da7faffb4
milenchaves <milenchaves.study@gmail.com>
2025-07-19 22:45:48 -0300

    feat: sobre nos home

---
+++ src/components/sobre/Sobre.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Sobre.css";
+import logo from "../../assets/image.png"; 
+
+const SobreNos = () => {
+    return (
+        <section className="sobre-nos">
+        <div className="sobre-nos-container">
+            <img src={logo} alt="Logo Amados" className="logo-sobre-nos" />
+            <div className="texto-sobre-nos">
+            <h2>Sobre nós</h2>
+            <p>
+                Somos uma ONG de Ocara-CE. Formada 
+                exclusivamente por voluntários, a Amados
+                realiza ações de resgate, castração, 
+                tratamento e busca por adoções responsáveis,
+                enfrentando sérias limitações de tempo, estrutura e recursos.
+            </p>
+            <button className="botao-sobre-nos">Saiba mais</button>
+            </div>
+        </div>
+        </section>
+    );
+};
+
+export default SobreNos;
