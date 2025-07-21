e74ee29f5131a09012a14933911741f390aec328
letdnts <mleticiadantas@alu.ufc.br>
2025-07-21 10:15:44 -0300

    feat: doe

---
+++ src/components/doe/doeHeader.jsx
@@ -0,0 +1,18 @@
+import './doe.css'
+import doe from '../../assets/doe.jpeg'
+
+export default function DoacaoHeader() {
+  return (
+    <div className="doacao-header">
+      <img src={doe} alt="Cachorro e Gato" />
+      <h2>
+        <strong>Faça uma doação</strong> e<br />
+        transforme a vida de quem<br />
+        não consegue pedir ajuda
+      </h2>
+    </div>
+  );
+}
+
+
+
