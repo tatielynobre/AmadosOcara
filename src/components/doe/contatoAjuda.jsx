f42d8653d15bafd02587f5472611a2694189dfb2
letdnts <mleticiadantas@alu.ufc.br>
2025-07-22 12:12:36 -0300

    fix: email icon

--- src/components/doe/contatoAjuda.jsx
+++ src/components/doe/contatoAjuda.jsx
@@ -1,10 +1,11 @@
 import './doe.css'
+import mailIcon from '../../assets/mail.png';
 
 export default function ContatoAjuda() {
   return (
     <div className="contato-ajuda">
       <h3><strong>Quer ajudar de outra maneira?</strong></h3>
-      <p>📧 E-mail: contato@Amados.org.br</p>
+      <p><img src={mailIcon} alt="E-mail" style={{ width: 22, verticalAlign: 'middle', marginRight: 8 }} />E-mail: contato@Amados.org.br</p>
     </div>
   );
 }
